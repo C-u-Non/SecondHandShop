acd8de24ae45a2514747761076d77af2e93003ec
Hồ Ngọc Hoàng Long <51793029+longlong7211@users.noreply.github.com>
2021-11-15 14:21:26 +0700

    fix cau truc

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express'); //import thư viện express
 var app = express(); //khởi tạo đối tượng app
 
 app.set('view engine', 'ejs');  //set view engine to ejs
-app.set("views",'component');  //set views to component
+app.set("views",'views');  //set views to component
 app.use(express.static("public"));  //set static path to public folder
 
 app.listen(3000); //start listening on port 3000
