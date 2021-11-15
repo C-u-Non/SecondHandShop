c60bed60950a479ea433e15b288f949428acc545
Nguyen Phu Cuong <nguyenncuongg9999@gmail.com>
2021-11-15 14:03:09 +0700

    Tạo task mới

---
+++ public/client.js
@@ -0,0 +1,15 @@
+var socket = io.connect('http://localhost:3000');
+socket.on('connect', function(data){
+    socket.emit('join','Nguoi dung vua moi truy cap');
+});
+
+socket.on('thress',function(data){
+    $('#thread').append('<li>' + data + '<li>')
+});
+
+$('form').submit(function(){
+    var message = $('message').val();
+    socket.emit('message',message);
+    this.reset();
+    return false;
+});
---
+++ public/js/scripts.js
@@ -0,0 +1,41 @@
+/*!
+* Start Bootstrap - Shop Homepage v5.0.3 (https://startbootstrap.com/template/shop-homepage)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+document.addEventListener("DOMContentLoaded", function(){
+    // make it as accordion for smaller screens
+    if (window.innerWidth < 992) {
+    
+      // close all inner dropdowns when parent is closed
+      document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
+        everydropdown.addEventListener('hidden.bs.dropdown', function () {
+          // after dropdown is hidden, then find all submenus
+            this.querySelectorAll('.submenu').forEach(function(everysubmenu){
+              // hide every submenu as well
+              everysubmenu.style.display = 'none';
+            });
+        })
+      });
+    
+      document.querySelectorAll('.dropdown-menu a').forEach(function(element){
+        element.addEventListener('click', function (e) {
+            let nextEl = this.nextElementSibling;
+            if(nextEl && nextEl.classList.contains('submenu')) {	
+              // prevent opening link if link needs to open dropdown
+              e.preventDefault();
+              if(nextEl.style.display == 'block'){
+                nextEl.style.display = 'none';
+              } else {
+                nextEl.style.display = 'block';
+              }
+    
+            }
+        });
+      })
+    }
+    // end if innerWidth
+    }); 
+    // DOMContentLoaded  end
